af1ddf64d534fe55639bd0f7b68caec9f59a1628
JungDongWook <jdwdonga21@zeroweb.kr>
2024-05-28 18:36:56 +0900

    불필요한 코드, 주석 제거

--- src/components/CalendarModal.tsx
+++ src/components/CalendarModal.tsx
@@ -116,7 +116,6 @@ class DateCalendarReferenceDate extends React.Component<DateCalendarProps, DateC
   handlePrevMonth = () => {
     this.setState((prevState) => ({
       displayedMonth: dayjs(subMonths(prevState.displayedMonth.toDate(), 1)),
-      // referenceDate: dayjs(subMonths(prevState.referenceDate.toDate(), 1)),
     }));
     const calendarApi = this.calendarRef.current?.getApi();
     calendarApi?.prev();
@@ -125,7 +124,6 @@ class DateCalendarReferenceDate extends React.Component<DateCalendarProps, DateC
   handleNextMonth = () => {
     this.setState((prevState) => ({
       displayedMonth: dayjs(addMonths(prevState.displayedMonth.toDate(), 1)),
-      // referenceDate: dayjs(addMonths(prevState.referenceDate.toDate(), 1)),
     }));
     const calendarApi = this.calendarRef.current?.getApi();
     calendarApi?.next();
@@ -147,7 +145,6 @@ class DateCalendarReferenceDate extends React.Component<DateCalendarProps, DateC
     if(!this.props.isModalOpenR){
       this.props.onOpenR();
     }
-    // alert(`Selected date: ${selectInfo.startStr}`);
     toast.success(`선택한 날짜 : ${selectInfo.startStr}`, ReactToastifyOptions);
   };
 
@@ -167,11 +164,6 @@ class DateCalendarReferenceDate extends React.Component<DateCalendarProps, DateC
       }
     }, 0);
   };
-  
-  
-
-
-  
 
   render() {
     const { displayedMonth } = this.state;
@@ -283,93 +275,6 @@ class DateCalendarReferenceDate extends React.Component<DateCalendarProps, DateC
           {/* 이동한 이벤트 리스트 */}
           {this.props.isModalOpenR && (
             <EventModal onCloseR={this.props.onCloseR} events={this.props.events} selectedDate={this.state.selectedDate}   />
-          //   <Box 
-          //   sx={{ 
-          //       position: 'absolute', 
-          //       top: '50%', 
-          //       left: '85%', 
-          //       transform: 'translate(-50%, -50%)', 
-          //       width: 400, 
-          //       height: '70vh', // 스크롤을 활성화하려면 모달의 고정 높이를 설정하세요.
-          //       bgcolor: 'background.paper', 
-          //       p: 2, 
-          //       overflow: 'hidden', // 모달 상자 자체에서 오버플로 숨기기
-          //       display: 'flex',
-          //       flexDirection: 'column',
-          //       borderRadius: 3,
-          //   }}
-          // >
-          //   <AppBar
-          //       position="static"
-          //       color="default"
-          //       elevation={0}
-          //       sx={{
-          //         width: '100%',
-          //         height: '7vh',
-          //         bgcolor: (theme) => theme.palette.mode === 'dark' ? '#1e1e1e' : '#ffffff',
-          //         borderBottom: '1px solid',
-          //         justifyContent: 'center',
-          //         position: 'fixed',
-          //         top: 0,
-          //         left: 0,
-          //         display: 'flex',
-          //         boxShadow: 3,                  
-          //       }}
-          //   >
-          //       <Toolbar>
-          //           <Typography fontWeight={900} variant="h6" component="h2" gutterBottom sx={{ flexGrow: 1, marginTop: 1, marginBottom: 1, fontSize: 22 }}>
-          //               {format(selectedDate.toDate(), 'yyyy.MM.dd')}
-          //           </Typography>
-          //           <div onClick={onCloseR} style={{ cursor: 'pointer' }}>
-          //               <CloseIcon />
-          //           </div>
-          //       </Toolbar>
-          //   </AppBar>
-          //   <Box sx={{ flexGrow: 1, overflow: 'auto', marginLeft: 1, marginRight: 1, marginTop: '8vh', marginBottom: '1vh', width: '95%', mb: 2, paddingRight: 1 }}>
-          //       {filteredEvents.map(event => (
-          //           <Card key={event.id} variant="outlined" sx={{ display: 'flex', marginBottom: 1, boxShadow: 3, alignItems: 'center'}}>
-          //               <Box sx={{ display: 'flex', flexDirection: 'column', flex: 5 }}>
-          //                   <CardContent sx={{flex: '1 0 auto' }}>
-          //                       <Typography component="div" fontWeight={900} fontSize={15} sx={{maxWidth: 150}}>
-          //                           {`${event.user_uuid}`}
-          //                       </Typography>
-          //                       <Typography variant="subtitle1" fontSize={13} component="div">
-          //                           {"+82 1055359909"}
-          //                       </Typography>
-          //                       {event.end_date !== null ? 
-          //                         <Typography variant="subtitle1" fontSize={11} component="div">
-          //                           {`${format(event.start_date,'yyyy-MM-dd')} - ${format(event.end_date, 'yyyy-MM-dd')}`}
-          //                         </Typography>
-          //                       :
-          //                         <Typography variant="subtitle1" fontSize={11} component="div">
-          //                           {`${format(event.start_date,'yyyy-MM-dd')} - '무기한'}`}
-          //                         </Typography>
-          //                       }
-                                
-          //                   </CardContent>
-          //               </Box>
-          //               <CardContent sx={{ display: 'flex', flex: 1 }}>
-          //                 {event.reason === null ? 
-          //                   <Typography component="div" fontWeight={900} variant="h5">
-          //                     없음
-          //                   </Typography>
-          //                  :
-          //                   event.reason === "사망" ? 
-          //                     <Typography component="div" fontWeight={900} color="red" variant="h5">
-          //                         {`${event.reason}`}
-          //                     </Typography>
-          //                   :
-          //                     <Typography component="div" fontWeight={900} variant="h5">
-          //                         {`${event.reason}`}
-          //                     </Typography>     
-          //                 }
-                                                  
-          //               </CardContent>
-          //           </Card>                 
-          //       ))}
-                
-          //   </Box>
-          // </Box>
           )}
         </ThemeProvider>
       </Modal>
